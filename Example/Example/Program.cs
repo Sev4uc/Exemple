c47c841ede06a778614dd41d4fe016aec364af30
Sev4uc <sev4uc.a@gmail.com>
2018-12-17 21:26:13 +0200

    My first commit

---
+++ Example/Example/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+using System;
+
+namespace Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Word");
+
+
+            Console.ReadLine();
+        }   
+    }
+}
